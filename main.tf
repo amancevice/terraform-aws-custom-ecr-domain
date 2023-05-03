7d2e7b62dfcb49b13f4759296169fdf4e04f4183
Alexander <alexander.mancevice@hey.com>
2023-05-03 08:13:06 -0500

    remove unused required provider

--- main.tf
+++ main.tf
@@ -6,11 +6,6 @@ terraform {
   required_version = "~> 1.0"
 
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
-
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
