fa619b330e103ba0917ef55351d92f91a1b5c717
Alexander <alexander.mancevice@hey.com>
2023-05-25 20:04:59 -0400

    remove terraform config from example

--- example/terraform.tf
+++ example/terraform.tf
@@ -1,16 +1,3 @@
-#################
-#   TERRAFORM   #
-#################
-
-terraform {
-  required_version = "~> 1.0"
-
-  required_providers {
-    archive = { source  = "hashicorp/archive" }
-    aws     = { source  = "hashicorp/aws" }
-  }
-}
-
 ###########
 #   AWS   #
 ###########
