d4eacc04bd01c85d3dd5d0c4ef5ad0abf8810130
Alexander Mancevice <alexander.mancevice@hey.com>
2024-11-23 12:26:39 -0500

    upgrade Lambda runtime to nodejs22.x

--- variables.tf
+++ variables.tf
@@ -101,7 +101,7 @@ variable "function_role_name" {
 variable "function_runtime" {
   description = "ECR custom domain proxy function runtime"
   type        = string
-  default     = "nodejs20.x"
+  default     = "nodejs22.x"
 }
 
 ###########
